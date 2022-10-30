59cd045f3dd71586887512ceca1a67857b5bc3ff
Abdull Shahmar <87323265+abdullZiba@users.noreply.github.com>
2022-10-30 14:14:41 +0300

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,67 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const StatoscopePlugin = require('@statoscope/webpack-plugin').default
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
+  entry: {
+    about: './src/pages/About.js',
+    home: './src/pages/Home.js',
+    main: {
+      dependOn: 'about',
+      import: './src/index.js'
     },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new StatoscopePlugin({
-            saveStatsTo: 'stats.json',
-            saveOnlyStats: false,
-            open: false,
-        }),
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-    },
-    module: {
-        rules: [
-            // @TODO js rule
-            // @TODO css rule
-        ],
-    },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
-};
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'dist'),
+      
+    }),
+    new StatoscopePlugin({
+      saveStatsTo: 'stats.json',
+      saveOnlyStats: false,
+      open: false,
+    }),
 
-module.exports = config;
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  target: 'web',
+    resolve: {
+      fallback: {
+        'crypto': require.resolve('crypto'),
+      },
+      alias: {
+        'crypto-browserify': path.resolve(__dirname, 'src/crypto-fallback.js'),
+        'react-is': path.resolve(__dirname, 'node_modules/react-is/cjs/react-is.production.min.js')
+      },
+    },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true
+  },
+}
+
+module.exports = config
